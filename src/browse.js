1d9bd4af48adf6a2959a9255b1f239c77e2a9428
Peter Nguyen <nguyepe2@flip2.engr.oregonstate.edu>
2018-06-09 21:06:01 -0700

    Added opening modal functionality to browse.html images

--- src/browse.js
+++ src/browse.js
@@ -1,4 +1,65 @@
 /*  Filename:    Browse.js
-    Description: This is the js file related to browsing the drawing previously saved by the user.
+    Description: This is the js file related to browsing the twit previously saved by the user.
 */
 
+
+var allTwits = [];
+
+function showCreateTwitModal() {
+
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var createTwitModal = document.getElementById('create-twit-modal');
+
+  // Show the modal and its backdrop.
+  modalBackdrop.classList.remove('hidden');
+  createTwitModal.classList.remove('hidden');
+}
+
+ function hideCreateTwitModal() {
+
+var modalBackdrop = document.getElementById('modal-backdrop');
+var createTwitModal = document.getElementById('create-twit-modal');
+
+   // Hide the modal and its backdrop.
+   modalBackdrop.classList.add('hidden');
+createTwitModal.classList.add('hidden');
+ }
+
+
+function parseTwitElem(twitElem) {
+  
+  var twit = {};
+
+  var twitTextElem = twitElem.querySelector('.drawing-text');
+  twit.text = twitTextElem.textContent.trim();
+
+  return twit;
+}
+
+
+
+
+var twitElemsCollection = document.getElementsByClassName('drawing');
+     for (var i = 0; i < twitElemsCollection.length; i++) {
+       allTwits.push(parseTwitElem(twitElemsCollection[i]));
+     }
+
+
+
+
+var createTwitButton = document.getElementsByClassName("drawing");
+  if (createTwitButton) {
+     for(var i=0; i < allTwits.length; i++) {
+    createTwitButton[i].addEventListener('click', showCreateTwitModal);
+     }
+  }
+
+var modalCloseButton = document.querySelector('#create-twit-modal .modal-close-button');
+  if (modalCloseButton) {
+    modalCloseButton.addEventListener('click', hideCreateTwitModal);
+  }
+
+  var modalCancalButton = document.querySelector('#create-twit-modal .modal-cancel-button');
+  if (modalCancalButton) {
+    modalCancalButton.addEventListener('click', hideCreateTwitModal);
+  }
