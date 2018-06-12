f24efa984143def927deb2e56b0d37dc722f5822
Jong Park <parkj6@oregonstate.edu>
2018-06-12 01:33:47 -0700

    server.js and json files to get node server going.

---
+++ server.js
@@ -0,0 +1,64 @@
+/* Filename: Server.js 
+   description: It serves.
+
+*/
+
+var path = require("path");
+var fs = require("fs");
+var http = require("http");
+var express = require("express");
+var exp_handlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var pictures = require("./photo-init");
+
+var app = express();
+app.engine("handlebars", exp_handlebars({defaultlayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.json());
+
+console.log("Starting express server...");
+
+app.get("/", function(req, res, err) {
+    var args = {
+        picture: pictures
+    };
+});
+
+app.get("/pictures", function(req, res, err){
+    var args = {
+        picture: pictures
+    };
+});
+
+app.post("/pictures/addPicture", function(req, res, err){
+    console.log("Adding Picture...");
+    var newPicture = {
+        title: req.body.title,
+        author: req.body.author,
+        password: req.body.password,
+//        tags: req.body.tags
+    };
+    pictures[newPicture.title] = newPicture;
+    console.log("Title: ", req.body.title);
+    
+    fs.writeFile("pictures.json", JSON.stringify(pictures), function(err) {
+        if (err) {
+            req.status(500).send("Unable to save new picture.");
+        } else {
+            res.status(200).send("Picture saved!");
+        }
+    });
+});
+
+app.use(express.static("/"));
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(8080, () => console.log("Server running on port 8080..."))
+
+/*if (processs.env.PORT) {
+    app.listen(PORT);
+} else {
+    app.listen(8080);
+}*/
