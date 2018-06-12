1c83cb597f143bcb4ccc5151ef1f09ccc94f7481
Jong Park <parkj6@oregonstate.edu>
2018-06-12 01:49:46 -0700

    Updated package and server.js

--- server.js
+++ server.js
@@ -52,13 +52,11 @@ app.post("/pictures/addPicture", function(req, res, err){
     });
 });
 
-app.use(express.static("/"));
+//app.use(express.static("/"));
+// Default view = index.html
+app.use (function(req, res) {
+  res.send("index.html");
+});
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(8080, () => console.log("Server running on port 8080..."))
-
-/*if (processs.env.PORT) {
-    app.listen(PORT);
-} else {
-    app.listen(8080);
-}*/
