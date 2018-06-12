a1f9905eac8177feccfdcecefe08b986b61b9af8
Carson Hannibal <carshann.gm@gmail.com>
2018-06-11 23:42:45 -0700

    Adding changes to draw.js and sketchpad.js

---
+++ draw.js
@@ -0,0 +1,9 @@
+/*  Filename:    Draw.js
+    Description: This is the js file related to users drawing on a clean template. This file will also contain the canvas for users to draw on.
+*/
+
+var sketchpad = new Sketchpad({
+  element: '#sketchpad',
+  width: 600,
+  height: 600
+});
