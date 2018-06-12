2965e4e041b3bbca026305577bd121a8ef2c89ae
Carson Hannibal <carshann.gm@gmail.com>
2018-06-12 01:04:35 -0700

    Adding sketchpad functionality

--- draw.js
+++ draw.js
@@ -7,3 +7,33 @@ var sketchpad = new Sketchpad({
   width: 600,
   height: 600
 });
+
+var undoButton = document.getElementById('undo-button');
+
+undoButton.addEventListener('click', function(event){
+  sketchpad.undo();
+});
+
+var redoButton = document.getElementById('redo-button');
+
+redoButton.addEventListener('click', function (event){
+  sketchpad.redo();
+});
+
+var colorButton = document.getElementById('color-button');
+
+colorButton.addEventListener('change', function (event){
+  sketchpad.color = colorButton.value;
+});
+
+var strokeSize = document.getElementById('myRange');
+
+strokeSize.addEventListener('change', function (event){
+  sketchpad.penSize = strokeSize.value;
+});
+
+var animateButton = document.getElementById('animate-button');
+
+animateButton.addEventListener('click', function(event){
+  sketchpad.animate(10);
+});
