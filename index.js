c331761db15ebb354ee457e8b1bec806cef90726
Jong Park <parkj6@oregonstate.edu>
2018-06-12 03:16:46 -0700

    node index.js

---
+++ index.js
@@ -0,0 +1,84 @@
+/* Filename: Index.js 
+   description: It serves.
+*/
+
+var debug = 1;
+var path = require("path");
+var fs = require("fs");
+const http = require("http");
+var express = require("express");
+var checkMimeType = true;
+
+const app = express();
+
+function requestHandler(req, res){
+  
+// Default view = index.html
+  if (req.url === '/'){
+    var filename = "/index.html";
+  } else {
+    var filename = req.url || "index.html";
+  }
+  var ext = path.extname(filename);
+  var localPath = __dirname;
+  var validExtentions = {
+    ".html" : "text/html",
+    ".js": "application/javascript",
+    ".css": "text/css",
+    ".txt": "text/plain",
+    ".jpg": "image/jpeg",
+    ".gif": "image/gif",
+    ".png": "image/png",
+  };
+  
+  var validMimeType = true;
+  var mimeType = validExtentions[ext];
+  if (checkMimeType){
+    validMimeType = validExtentions[ext] != undefined;
+  }
+  
+  if (validMimeType) {
+    localPath += filename;
+    fs.exists(localPath, function(exists) {
+      if (exists) {
+        console.log("File Downloaded:" + localPath);
+        getFile(localPath, res, mimeType);
+        res.statusCode = 200;
+      } else {
+        console.log("File not found:" + localPath);
+        res.writeHead(404);
+        res.end();
+      } 
+    });
+  }
+  
+  if (debug) {
+    console.log("----------------------");
+    console.log("Debug Enabled...");
+    console.log("Method: ", req.method);
+    console.log("URL: ", req.url);
+    console.log("Status Code: ", res.statusCode);
+  }
+}
+function getFile(localPath, res, mimeType) {
+  fs.readFile(localPath, function(err, contents) {
+    if(!err) {
+      res.setHeader("Content-Length", contents.length);
+      if (mimeType != undefined) {
+        res.setHeader("Content-Type", mimeType);
+      }
+      res.statusCode = 200;
+      res.end(contents);
+    } else {
+      res.writeHead(500);
+      res.end();
+    }
+  });
+}
+
+var server = http.createServer(requestHandler);
+var PORT = 8080;
+
+server.listen(PORT, function () {
+  console.log("Server running on port "+PORT+"...");
+});
