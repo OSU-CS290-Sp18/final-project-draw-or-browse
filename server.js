b60ac0f710f8bf6422d300dbd368fb006771d0ec
Jong Park <parkj6@oregonstate.edu>
2018-06-14 05:26:12 -0700

    Fresh server.js /test works.

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
    description: It serves you right.
    */
 
-var debug = 1; //Enable to see status code
+var DEBUG = true; // Enable to see Status Code. 
+var PORT = 9154;
 var path = require("path");
 var fs = require("fs");
 const http = require("http");
 var express = require("express");
 var exphbs = require("express-handlebars");
-var checkMimeType = true;
 var drawingData = require("./photo-init");
 
+/////////////////
+// Mongo Stuff //
+/////////////////
 var MongoClient = require('mongodb').MongoClient;
 var mongoHost = process.env.MONGO_HOST;
 var mongoPort = process.env.MONGO_PORT || 27017;
@@ -26,116 +29,56 @@ var mongoDBDatabase;
 
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+var homePage = fs.readFileSync('public/index.html', 'utf8');
+var drawPage = fs.readFileSync('public/draw.html', 'utf8');
+var browsePage = fs.readFileSync('public/browse.html', 'utf8');
+
+//setting up handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// Public view
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-   res.status(200).render('partials/indexPage');
-   /*res.status(200).render('browsePage', {
-     data: drawingData}); */
+// Load up Index Page 
+app.get('/', function (req, res, next) {
+  res.status(200).render('index');
+  console.log("==Index Page Handlebar loaded.");
 });
 
-app.get('/browse.html', function (req, res) {
-   res.status(200).render('browsePage', {
-     data: drawingData});
+// Load up Draw Page 
+app.get('/draw.html', function (req, res, next) {
+  res.status(200).render('draw');
+  console.log("==Draw Page Handlebar loaded.");
 });
 
-app.get('/draw.html', function (req, res) {
-   res.status(200).render('drawPage');
+// Load up Browse Page 
+app.get('/browse.html', function (req, res, next) {
+  res.status(200).render('browse', {
+    pictureObjects : drawingData
+  });
+  console.log("==Browse Page Handlebar loaded.")
 });
 
-/*
-function requestHandler(req, res){
-
-   // Default view = index.html
-   if (req.url === '/'){
-      var filename = "/index.html";
-   } else {
-      var filename = req.url || "index.html";
-   }
-   var ext = path.extname(filename);
-   var localPath = __dirname;
-   var validExtentions = {
-      ".html" : "text/html",
-      ".htm" : "text/html",
-      ".js": "application/javascript",
-      ".css": "text/css",
-      ".txt": "text/plain",
-      ".jpg": "image/jpeg",
-      ".gif": "image/gif",
-      ".png": "image/png",
-   };
-
-   var mimeType = validExtentions[ext];
-   if (checkMimeType){
-      validMimeType = validExtentions[ext] != undefined;
-   }
-
-  if (validMimeType) {
-    localFile = localPath +filename;
-    fs.exists(localFile, function(exists) {
-      if (exists) {
-        console.log("File Downloaded:" + localFile);
-        res.statusCode = 200;
-      } else {
-        console.log("File not found:" + localFile);
-        //res.writeHead(404);
-        res.statusCode = 404;
-        filename = "/redir/404.html";
-        localFile = localPath +filename;
-      }
-      getFile(localFile, res, mimeType);
-    });
-  }
-	/////////////
-	////DEBUG////
-	/////////////
-  if (debug) {
-    console.log("----------------------");
-    console.log("Debug Enabled...");
-    console.log("Method: ", req.method);
-    console.log("URL: ", req.url);
-    console.log("Status Code: ", res.statusCode);
-  }
-}
-function getFile(localPath, res, mimeType) {
-   fs.readFile(localPath, function(err, contents) {
-      if(!err) {
-	 res.setHeader("Content-Length", contents.length);
-	 if (mimeType != undefined) {
-	    res.setHeader("Content-Type", mimeType);
-	 }
-	 res.statusCode = 200;
-	 res.end(contents);
-      } else {
-	 res.writeHead(500);
-	 res.end();
-      }
-   });
-}
-
-var server = http.createServer(requestHandler); */
-var PORT = 9154;
-
-app.get('*', function (req, res) {
-   res.status(404);
-   res.send("The page you requested doesn't exist");
-});
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-//Mongo Stuff 
-
-MongoClient.connect(mongoURL, function (err, client) {
-   if (err) {
-      throw err;
-   }
-   db = mongoDBDatabase = client.db(mongoDBName);
-
-   // Starting Server
-   app.listen(PORT, function () {
-      console.log("Server running on port "+PORT+"...");
-   });
+/////////////////////////////
+// Testing the Browse Page //
+/////////////////////////////
+app.get('/test', function (req, res, next) {
+  res.render('browse', {
+    test: [ 
+    {
+      title: "Apple",
+      author: "Apple Author",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Honeycrisp.jpg/1280px-Honeycrisp.jpg?1528813883629",
+    }
+  ]
+  });
+  console.log("==Testing partial Handlebar loaded.");
 });
 
+/////////////////////////
+// Starting the Server //
+/////////////////////////
+app.listen(PORT, function(){
+  console.log("==Server running on port "+PORT+"...");
+});
